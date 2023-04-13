b80f9e5682ca82795f7f204c836df58ba0617f30
Pau-examen <99324319+alexisescobales@users.noreply.github.com>
2023-04-13 13:49:36 +0200

    mapa

--- resources/views/Mapa/mapa.blade.php
+++ resources/views/Mapa/mapa.blade.php
@@ -2,10 +2,17 @@
 
 @section('title', 'Mapa')
 
-@section('mapacss')
-    <link rel="stylesheet" href="{{ asset('css/mapa.css') }}">
+@section('hrefmap')
+<link rel="stylesheet" href="{{ asset('css/mapa.css') }}">
+<link rel="stylesheet" href="{{ asset('js/mapa.js') }}">
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
 @endsection
 
-@section('mapajs')
-    <link rel="stylesheet" href="{{ asset('js/mapa.js') }}">
+@section('content')
+<body>
+    <div id="map">
+
+    </div>
+</body>
 @endsection
