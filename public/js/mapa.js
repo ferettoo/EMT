938e5708f93901f36406fa5bc2eb41f99a7cc42f
Alexis Escobales <aescobalesg2122@politecnics.barcelona>
2023-04-25 10:03:43 +0200

    mapa

--- public/js/mapa.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGlzcG9saXRlY25pY3MiLCJhIjoiY2xnZjI0bDhiMDlqMDNucXBwZ2tncjcwYiJ9.cyNA9AcMvwC8spRryojQlA';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
-
